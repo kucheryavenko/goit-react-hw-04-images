fbcf74f61111b3a9862589a72cdef79fb735fea9
Oleksandr Kucheryavenko <kucheryavenko.oleksandr@gmail.com>
2022-10-05 23:29:35 +0300

    add hook App
    component App from class to hook

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,5 @@
-import { Component } from 'react';
+// import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import {
@@ -11,87 +12,153 @@ import {
 } from 'components';
 import { fetchData } from 'services/api';
 
-export class App extends Component {
-  state = {
-    hits: [],
-    searchQuery: '',
-    page: 1,
-    status: 'idle',
-  };
+export const App = () => {
+  const [hits, setHits] = useState([]);
+  const [searchQuery, setSearchQuery] = useState('');
+  const [page, setPage] = useState(1);
+  const [status, setStatus] = useState('idle');
 
-  componentDidUpdate(_, prevState) {
-    if (
-      prevState.searchQuery !== this.state.searchQuery ||
-      prevState.page !== this.state.page
-    ) {
-      this.getImage(this.state.searchQuery, this.state.page);
-    }
-  }
-
-  getImage = async (searchQuery, page) => {
-    if (!searchQuery) {
-      return;
-    }
-    try {
-      this.setState({ status: 'pending' });
-
-      const { hits, totalHits } = await fetchData(searchQuery, page);
-
-      if (hits.length === 0) {
-        toast.error(
-          'Sorry, there are no images matching your search query. Please try again.'
-        );
-        this.setState({ status: 'rejected' });
+  useEffect(() => {
+    const getImage = async (searchQuery, page) => {
+      if (!searchQuery) {
         return;
       }
-      if (page === 1) {
-        toast.success(`Hooray! We found ${totalHits} images.`);
+      try {
+        setStatus('pending');
+
+        const { hits, totalHits } = await fetchData(searchQuery, page);
+
+        if (hits.length === 0) {
+          toast.error(
+            'Sorry, there are no images matching your search query. Please try again.'
+          );
+          setStatus('rejected');
+          return;
+        }
+        if (page === 1) {
+          toast.success(`Hooray! We found ${totalHits} images.`);
+        }
+
+        setStatus('resolved');
+        setHits(prevHits => [...prevHits, ...hits]);
+      } catch (error) {
+        console.error(error);
+        setStatus('rejected');
       }
-      this.setState({ status: 'resolved' });
-      this.setState(prevState => {
-        return {
-          hits: [...prevState.hits, ...hits],
-        };
-      });
-    } catch (error) {
-      console.error(error);
-      this.setState({ status: 'rejected' });
-    }
+    };
+
+    getImage(searchQuery, page);
+  }, [searchQuery, page]);
+
+  const handleFormSubmit = searchQuery => {
+    setSearchQuery(searchQuery);
+    setHits([]);
+    setPage(1);
   };
 
-  handleFormSubmit = searchQuery => {
-    this.setState({ searchQuery, page: 1, hits: [] });
+  const onLoadMore = () => {
+    setPage(prevPage => prevPage + 1);
   };
 
-  onLoadMore = () => {
-    this.setState(prevState => {
-      return {
-        page: prevState.page + 1,
-      };
-    });
-  };
+  return (
+    <Container>
+      <Searchbar onSubmit={handleFormSubmit} />
+      {status === 'rejected' && (
+        <Notification>
+          Ooops, someting went wrong. Please, try again.
+        </Notification>
+      )}
+      {hits.length > 0 && status !== 'rejected' && <ImageGallery hits={hits} />}
+      {status === 'pending' && <Loader />}
+      {status === 'resolved' && hits.length > 0 && hits.length % 12 === 0 && (
+        <Button onClick={onLoadMore}>LoadMore</Button>
+      )}
+      <ToastContainer autoClose={5000} />
+    </Container>
+  );
+};
 
-  render() {
-    const { hits, status } = this.state;
-    const { handleFormSubmit, onLoadMore } = this;
+// export class App extends Component {
+//   state = {
+//     hits: [],
+//     searchQuery: '',
+//     page: 1,
+//     status: 'idle',
+//   };
 
-    return (
-      <Container>
-        <Searchbar onSubmit={handleFormSubmit} />
-        {status === 'rejected' && (
-          <Notification>
-            Ooops, someting went wrong. Please, try again.
-          </Notification>
-        )}
-        {hits.length > 0 && status !== 'rejected' && (
-          <ImageGallery hits={hits} />
-        )}
-        {status === 'pending' && <Loader />}
-        {status === 'resolved' && hits.length > 0 && hits.length % 12 === 0 && (
-          <Button onClick={onLoadMore}>LoadMore</Button>
-        )}
-        <ToastContainer autoClose={5000} />
-      </Container>
-    );
-  }
-}
+//   componentDidUpdate(_, prevState) {
+//     if (
+//       prevState.searchQuery !== this.state.searchQuery ||
+//       prevState.page !== this.state.page
+//     ) {
+//       this.getImage(this.state.searchQuery, this.state.page);
+//     }
+//   }
+
+//   getImage = async (searchQuery, page) => {
+//     if (!searchQuery) {
+//       return;
+//     }
+//     try {
+//       this.setState({ status: 'pending' });
+
+//       const { hits, totalHits } = await fetchData(searchQuery, page);
+
+//       if (hits.length === 0) {
+//         toast.error(
+//           'Sorry, there are no images matching your search query. Please try again.'
+//         );
+//         this.setState({ status: 'rejected' });
+//         return;
+//       }
+//       if (page === 1) {
+//         toast.success(`Hooray! We found ${totalHits} images.`);
+//       }
+//       this.setState({ status: 'resolved' });
+//       this.setState(prevState => {
+//         return {
+//           hits: [...prevState.hits, ...hits],
+//         };
+//       });
+//     } catch (error) {
+//       console.error(error);
+//       this.setState({ status: 'rejected' });
+//     }
+//   };
+
+//   handleFormSubmit = searchQuery => {
+//     this.setState({ searchQuery, page: 1, hits: [] });
+//   };
+
+//   onLoadMore = () => {
+//     this.setState(prevState => {
+//       return {
+//         page: prevState.page + 1,
+//       };
+//     });
+//   };
+
+//   render() {
+//     const { hits, status } = this.state;
+//     const { handleFormSubmit, onLoadMore } = this;
+
+//     return (
+//       <Container>
+//         <Searchbar onSubmit={handleFormSubmit} />
+//         {status === 'rejected' && (
+//           <Notification>
+//             Ooops, someting went wrong. Please, try again.
+//           </Notification>
+//         )}
+//         {hits.length > 0 && status !== 'rejected' && (
+//           <ImageGallery hits={hits} />
+//         )}
+//         {status === 'pending' && <Loader />}
+//         {status === 'resolved' && hits.length > 0 && hits.length % 12 === 0 && (
+//           <Button onClick={onLoadMore}>LoadMore</Button>
+//         )}
+//         <ToastContainer autoClose={5000} />
+//       </Container>
+//     );
+//   }
+// }
