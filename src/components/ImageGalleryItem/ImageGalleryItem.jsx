17ceb35e737cfe6b9ac8e6afaed8f092c3681b65
Oleksandr Kucheryavenko <kucheryavenko.oleksandr@gmail.com>
2022-10-05 23:35:12 +0300

    clearing comments
    clearing comments in components

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,4 +1,3 @@
-// import { Component } from 'react';
 import { useState } from 'react';
 import PropTypes from 'prop-types';
 import { Item, Img } from 'components/ImageGalleryItem/ImageGalleryItem.styled';
@@ -30,39 +29,3 @@ ImageGalleryItem.propTypes = {
   largeImageURL: PropTypes.string.isRequired,
   tags: PropTypes.string.isRequired,
 };
-
-// export class ImageGalleryItem extends Component {
-//   state = {
-//     showModal: false,
-//   };
-
-//   toggleModal = () => {
-//     this.setState(({ showModal }) => ({
-//       showModal: !showModal,
-//     }));
-//   };
-
-//   render() {
-//     const { webformatURL, largeImageURL, tags } = this.props;
-//     const { showModal } = this.state;
-//     const { toggleModal } = this;
-//     return (
-//       <>
-//         <Item onClick={toggleModal}>
-//           <Img src={webformatURL} alt={tags} loading="lazy" />
-//         </Item>
-//         {showModal && (
-//           <Modal onClose={toggleModal}>
-//             <img src={largeImageURL} alt={tags} />
-//           </Modal>
-//         )}
-//       </>
-//     );
-//   }
-// }
-
-// ImageGalleryItem.propTypes = {
-//   webformatURL: PropTypes.string.isRequired,
-//   largeImageURL: PropTypes.string.isRequired,
-//   tags: PropTypes.string.isRequired,
-// };
--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,4 +1,3 @@
-// import { Component } from 'react';
 import { useEffect } from 'react';
 import { createPortal } from 'react-dom';
 import PropTypes from 'prop-types';
@@ -39,35 +38,3 @@ Modal.propTypes = {
   onClose: PropTypes.func.isRequired,
   children: PropTypes.object.isRequired,
 };
-
-// export class Modal extends Component {
-//   componentDidMount() {
-//     window.addEventListener('keydown', this.handleKeyDown);
-//   }
-
-//   componentWillUnmount() {
-//     window.removeEventListener('keydown', this.handleKeyDown);
-//   }
-
-//   handleKeyDown = evt => {
-//     if (evt.code === 'Escape') {
-//       this.props.onClose();
-//     }
-//   };
-
-//   handleBackdropClick = evt => {
-//     if (evt.currentTarget === evt.target) {
-//       this.props.onClose();
-//     }
-//   };
-
-//   render() {
-//     const { children } = this.props;
-//     return createPortal(
-//       <Overlay onClick={this.handleBackdropClick}>
-//         <ModalWindow>{children}</ModalWindow>
-//       </Overlay>,
-//       modalRoot
-//     );
-//   }
-// }
